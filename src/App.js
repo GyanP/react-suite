03c0970cae213df242106233b41e87c0c7575497
GyanP <gyanig72@gmail.com>
2021-08-23 15:42:48 +0530

    Install Rsuite in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
+import 'rsuite/dist/styles/rsuite-dark.css';
 
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
